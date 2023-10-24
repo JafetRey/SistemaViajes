00fdeb76c4e51c8ae7281842c8c22f6c0bf7511d
jafetrey10 <jafetrey10@gmail.com>
2023-10-24 01:17:30 -0600

    Colaborador_Sucursal Viajes

--- SistemaViajes/Controllers/HomeController.cs
+++ SistemaViajes/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using SistemaViajes.Recursos;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SistemaViajes.Servicios.Implementacion;
+using Microsoft.AspNetCore.Identity;
 
 namespace SistemaViajes.Controllers
 {
@@ -135,60 +136,135 @@ namespace SistemaViajes.Controllers
         public IActionResult CrearColaboradorSucursal()
         {
             // Verificar si hay registros de colaboradores y sucursales en la base de datos
-            if (!_context.Colaboradores.Any() || !_context.Sucursales.Any())
+            var colaboradores = _context.Colaboradores.ToList();
+            var sucursales = _context.Sucursales.ToList();
+
+            if (colaboradores.Count == 0 || sucursales.Count == 0)
             {
                 TempData["Mensaje"] = "Asegúrate de que haya registros de colaboradores y sucursales en la base de datos antes de crear una relación.";
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index");
             }
 
             // Obtener la lista de colaboradores y sucursales para mostrar en los dropdowns
-            ViewBag.Colaboradores = _context.Colaboradores.Select(c => new SelectListItem
-            {
-                Value = c.Id.ToString(),
-                Text = c.Nombre
-            }).ToList();
+            ViewBag.Colaboradores = new SelectList(colaboradores, "Id", "Nombre");
+            ViewBag.Sucursales = new SelectList(sucursales, "Id", "Nombre");
 
-            ViewBag.Sucursales = _context.Sucursales.Select(s => new SelectListItem
-            {
-                Value = s.Id.ToString(),
-                Text = s.Nombre
-            }).ToList();
-
-            return View();
+            return View(new ColaboradorSucursal());
         }
 
         [HttpPost]
         public IActionResult CrearColaboradorSucursal(ColaboradorSucursal modelo)
         {
+            // Validar el campo de distancia utilizando el atributo [Range]
             if (ModelState.IsValid)
             {
+                // Validar que el ID del colaborador y la sucursal sean números enteros
+                if (!int.TryParse(modelo.ColaboradorId.ToString(), out int colaboradorId) || !int.TryParse(modelo.SucursalId.ToString(), out int sucursalId))
+                {
+                    ModelState.AddModelError("ColaboradorId", "El ID del colaborador debe ser un número entero.");
+                    ModelState.AddModelError("SucursalId", "El ID de la sucursal debe ser un número entero.");
+                    return View(modelo);
+                }
+
+                // Guardar la relación en la base de datos
                 _context.ColaboradorSucursals.Add(modelo);
                 _context.SaveChanges();
 
-                ViewData["Mensaje"] = "Relación creada con éxito";
+                ViewData["Mensaje"] = "Relación Colaborador-Sucursal creada con éxito";
 
                 // Limpiar los campos del modelo para una nueva entrada
-                modelo.DistanciaKilometros = 0; // O establecer el valor predeterminado que desees
-
+                modelo = new ColaboradorSucursal();
                 return View(modelo);
             }
 
             // Obtener la lista de colaboradores y sucursales para mostrar en los dropdowns
-            ViewBag.Colaboradores = _context.Colaboradores.Select(c => new SelectListItem
-            {
-                Value = c.Id.ToString(),
-                Text = c.Nombre
-            }).ToList();
-
-            ViewBag.Sucursales = _context.Sucursales.Select(s => new SelectListItem
-            {
-                Value = s.Id.ToString(),
-                Text = s.Nombre
-            }).ToList();
+            ViewBag.Colaboradores = new SelectList(_context.Colaboradores, "Id", "Nombre");
+            ViewBag.Sucursales = new SelectList(_context.Sucursales, "Id", "Nombre");
 
             ViewData["Mensaje"] = "No se pudo crear la relación. Por favor, verifica los datos.";
             return View(modelo);
         }
+
+        public IActionResult RegistroViaje()
+        {
+
+            // Cargar la lista de sucursales
+            var sucursales = _context.Sucursales.ToList();
+            ViewBag.Sucursales = new SelectList(sucursales, "Id", "Nombre");
+
+            // Cargar la lista de transportistas
+            var transportistas = _context.Transportistas.ToList();
+            ViewBag.Transportistas = new SelectList(transportistas, "Id", "Nombre");
+
+            // Cargar la lista de colaboradores
+            var colaboradores = _context.Colaboradores.ToList();
+            ViewBag.Colaboradores = new SelectList(colaboradores, "Id", "Nombre");
+
+            // Obtener el usuario actual (gerente de tienda)
+            var usuarioActual = "UsuarioGerente"; // Reemplaza con la lógica para obtener el usuario actual
+
+            // Pasar el usuario al formulario
+            ViewBag.UsuarioRegistrador = usuarioActual;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistroViaje(Viaje modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                // Realiza las validaciones y el registro del viaje en la base de datos
+                var colaboradoresSeleccionados = _context.ColaboradorSucursals.Where(cs => modelo.ColaboradorId.HasValue && cs.ColaboradorId == modelo.ColaboradorId.Value).ToList();
+                var sumaDistancias = colaboradoresSeleccionados.Sum(cs => cs.DistanciaKilometros);
+
+                if (sumaDistancias > 100)
+                {
+                    ModelState.AddModelError("ColaboradorIds", "La suma de las distancias no debe superar los 100 km.");
+                    // Recarga la lista de sucursales y transportistas
+                    var sucursal = _context.Sucursales.ToList();
+                    ViewBag.Sucursales = new SelectList(sucursal, "Id", "Nombre");
+                    var transportista = _context.Transportistas.ToList();
+                    ViewBag.Transportistas = new SelectList(transportista, "Id", "Nombre");
+                    return View(modelo);
+                }
+
+                // Verificar si el colaborador ya viajó hoy (puedes implementar esta lógica según tus necesidades)
+
+                // Realizar el registro del viaje en la base de datos
+                // Obtener el nombre de usuario del usuario autenticado
+                var usuarioActualNombre = User.Identity.Name;
+                // Obtener el usuario actual
+                var usuarioActual = _context.Usuarios.FirstOrDefault(u => u.NombreUsuario == usuarioActualNombre);
+
+                if (usuarioActual != null)
+                {
+                    // Asignar el ID del usuario registrado al modelo
+                    modelo.UsuarioRegistradorId = usuarioActual.Id;
+                }
+                else
+                {
+                    // Manejo de error si no se encuentra el usuario
+                    ModelState.AddModelError(string.Empty, "No se pudo encontrar el usuario registrado.");
+                    return View(modelo);
+                }
+
+                _context.Viajes.Add(modelo);
+                _context.SaveChanges();
+
+                // Redirige a una página de éxito o muestra un mensaje de éxito
+                return RedirectToAction("RegistroExitoso");
+            }
+
+            // Si hay errores de validación, vuelve a mostrar el formulario
+            // Recarga la lista de sucursales y transportistas
+            var sucursales = _context.Sucursales.ToList();
+            ViewBag.Sucursales = new SelectList(sucursales, "Id", "Nombre");
+            var transportistas = _context.Transportistas.ToList();
+            ViewBag.Transportistas = new SelectList(transportistas, "Id", "Nombre");
+            return View(modelo);
+        }
+
 
         public async Task<IActionResult> CerrarSesion()
         {
